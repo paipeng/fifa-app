0ffb78ee672517726512ffbf07c9e9140a9234c3
Pai Peng <pai.peng@apparentmedia.de>
2015-05-29 16:17:10 +0200

    update

--- scripts/controllers/naviController.js
+++ scripts/controllers/naviController.js
@@ -29,5 +29,15 @@ fifaApp.controller('NavCtrl', ['$rootScope', '$scope',
             console.log("modules " + loaded_modules[0]);
 
         }
+
+        scope.showLine = function() {
+            console.log("showLine "  + rootScope.menuList.length);
+
+            if (rootScope.menuList && rootScope.menuList.length > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
     ]);
